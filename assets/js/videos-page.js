2b7714538c4daad82869d8615fff9620ce2cc832
Jayman X <jaymanx4life@gmail.com>
2017-09-01 18:44:39 +0800

    testing on Openshift

---
+++ assets/js/videos-page.js
@@ -0,0 +1,21 @@
+$(function whenDomIsReady(){
+    $('.the-list-of-videos .loading').show();
+
+    setTimeout(function afterRetrievingVideos() {
+        var videos = [{
+            title: 'FUNNY BABY VIDEOS',
+            src:    'https://youtube.com/embed/_FvTVWjLiHM'
+        },{
+            title: 'Justin Bieber - Baby ft. Ludacris',
+            src:    'https://youtube.com/embed/kffacxfA7G4'
+        },{
+            title: 'Charlie bit my finger - again',
+            src: 'https://youtube.com/embed/_OBlgSz8sSM'
+        }];
+        $('.the-list-of-videos .loading').hide();
+        
+        var videosHtml = _.reduce(videos, function(html, video){
+            
+        })
+    })
+})
