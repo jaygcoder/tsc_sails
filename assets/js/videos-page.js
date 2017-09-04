70835e6854de3bf54cdfb3a06f59095c8f200798
Jayman X <jaymanx4life@gmail.com>
2017-09-04 17:06:32 +0800

    Videos JS practice

--- assets/js/videos-page.js
+++ assets/js/videos-page.js
@@ -16,7 +16,12 @@ $(function whenDomIsReady(){
         
         var videosHtml = _.reduce(videos, function(html, video){
             html += '<li class="video">' +
-            '<h2>' + video.title + '<'
-        })
-    })
-})
+            '<h2>' + video.title + '</h2>' +
+            '<iframe width="640" height="390" src="' + video.src + '" ' +
+            'frameborder="0" allowfullscreen></iframe>' +
+            '</li>';
+            return html;
+        },'');
+        $('.the-list-of-videos ul').replaceWith(videosHtml);
+    }, 750);
+});
